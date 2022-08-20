2b38e7bf7ccfb28928d0c907f5c3ba2b5462abc9
AWSMProdigy <StraightAKyle@gmail.com>
2022-08-19 23:10:23 -0400

    Working on sending pm

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ dotenv.config();
 
 const client = new DiscordJS.Client({ intents: [GatewayIntentBits.Guilds ]});
 
+function sendMessage(role){
+    let role = message.guild.roles.find(r => r.name === "Batman");
+    let members = role.members.map(m=>m.user.id);
+    let index = Math.floor(Math.random() * members.length);
+    members[index].send("Hey, listen!");
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
+    setInterval(sendMessage, 1000);
 });
+
+
 
 // Login to Discord with your client's token
 client.login(token);
